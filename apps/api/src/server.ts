a203b71a7b46e8a185b621b771cfa6ad310a91bd
lmartns <leonardomartins578@gmail.com>
2025-08-09 16:31:39 -0300

    🚧 WIP: Swagger

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,7 +1,14 @@
 import { app } from "./app";
 import { AppDataSource } from "./infrastructure/database/data-source";
+import swaggerUi from "swagger-ui-express";
+import bodyParser from "body-parser";
+import swaggerFile from "../swagger-output.json";
+
 
 const port = process.env.PORT || 3333;
+
+app.use(bodyParser.json());
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 AppDataSource.initialize()
   .then(() => {
---
+++ apps/api/src/swagger.ts
@@ -0,0 +1,15 @@
+import swaggerAutogen from 'swagger-autogen';
+
+const outputFile = './swagger-output.json';
+const endpointsFiles = ['./src/infrastructure/http/routes/user.routes.ts'];
+
+const doc = {
+  info: {
+    title: 'API',
+    description: 'Documentation'
+  },
+  host: 'localhost:3000',
+  schemes: ['http']
+};
+
+swaggerAutogen({ openapi: '3.0.0' })(outputFile, endpointsFiles, doc);
